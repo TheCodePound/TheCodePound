a4c9125e4c586366bdf145d8bd0bedc5edc73476
paulbrooks646 <paulbrooks646@gmail.com>
2020-07-10 18:33:09 -0600

    store

---
+++ src/ducks/store.js
@@ -0,0 +1,11 @@
+import {createStore, applyMiddleware, combineReducers} from 'redux'
+import userReducer from './userReducer'
+import postReducer from './postReducer'
+import promiseMiddleware from 'redux-promise-middleware'
+
+const rootReducer = combineReducers({
+    user: userReducer,
+    posts: postReducer
+})
+
+export default createStore(rootReducer, applyMiddleware(promiseMiddleware))
