75eb4aed3d445edd2423f9fb6fce7742029c99d5
paulbrooks646 <paulbrooks646@gmail.com>
2020-07-10 20:02:39 -0600

    nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,96 @@
-import react, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {Link, withRouter} from 'react-router-dom'
 import {connect} from 'react-redux'
-imort 
+import {logoutUser, getUser} from '../ducks/userReducer'
+import axios from 'axios'
+import '../styles/App.scss'
+
+function Nav(props) {
+    
+    useEffect(() => {
+        getUser()
+    }, [])
+
+
+const logout = () => {
+    axios.delete('/api/logout')
+    .then(() => {
+        props.logoutUser()
+        props.history.push('/')
+    })}
+
+const [search, setSearch] = useState("")
+
+function siteSearch() {
+    console.log("search")
+}
+
+const pathname = props.location.pathname
+
+if (pathname === '/') {
+    return (
+        <div>
+            <div>
+                <img src="" alt="Dog Logo"/>
+            </div>
+            <div>
+                <a href="#about">
+                    <h2>About</h2>
+                </a>
+                <Link to="/Register">
+                    <h2>Sign In</h2>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+else if (pathname === '/Register') {
+    return (
+        <div>
+            <div>
+                <img src="" alt="Dog Logo"/>
+            </div>
+            <div>
+                <a href="#about">
+                    <h2>About</h2>
+                </a>
+                <Link to="/">
+                    <h2>Sign In</h2>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+else {
+    return (
+        <div>
+            <div>
+                <div>
+                    <img src="" alt="Dog Logo"/>
+                </div>
+                <div>
+                    <img 
+                        src="" 
+                        alt="Magnifying Glass"
+                        onClick={() => siteSearch()}
+                    />    
+                    <input 
+                        placeholder="Search Codepound"
+                        type="text"
+                        value={search}
+                        onChange={e => setSearch(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <button onClick={() => logout()}>Logout</button>
+                </div>
+            </div>
+        </div>
+    )
+}}
+
+const mapStateToProps = reduxState => reduxState
+
+export default withRouter(connect(mapStateToProps, {logoutUser, getUser})(Nav))
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Newpost from './components/Newpost.js'
 import Popup from './components/Popup.js'
 import Profile from './components/Profile'
 import Signin from './components/Signin.js'
+import Register from './components/Register.js'
 
 export default (
     <Switch>
@@ -13,5 +14,6 @@ export default (
         <Route path="/Newpost" component={Newpost}/>
         <Route path="/Popup/:post_id" component={Popup}/>
         <Route path="/Profile" component={Profile}/>
+        <Route path="/Register" component={Register}/>
     </Switch>
 )
