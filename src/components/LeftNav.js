0237a48737eb1be842a62bcb7ac6be31df646b49
paulbrooks646 <paulbrooks646@gmail.com>
2020-07-11 17:37:48 -0600

    flexbox

--- src/components/LeftNav.js
+++ src/components/LeftNav.js
@@ -31,13 +31,13 @@ function LeftNav(props) {
     else if (pathname === '/Profile') {
 
     return (
-        <div>
+        <div className="leftnavmain">
             <img 
                 src=""
                 alt="user"
             />
             <h2>username</h2>
-            <div>
+            <div className="left-nav-div">
                 <img
                     src=""
                     alt="person"
@@ -46,21 +46,21 @@ function LeftNav(props) {
                     <h2>Home</h2>
                 </Link>     
             </div>
-            <div>
+            <div div className="left-nav-div">
                 <img
                     src=""
                     alt="people"
                 />
                 <h2>Friends</h2>  
             </div>
-            <div>
+            <div div className="left-nav-div">
                 <img
                     src=""
                     alt="bone"
                 />
                 <h2>Dog Bones</h2>
             </div>
-            <div>
+            <div div className="left-nav-div">
                 <img
                     src=""
                     alt="plus"
@@ -109,13 +109,13 @@ function LeftNav(props) {
 
     else if (pathname === '/Home') {
         return (
-            <div>
+            <div className="left-nav-main">
                 <img 
                     src=""
                     alt="user"
                 />
                 <h2>username</h2>
-                <div>
+                <div className="left-nav-div">
                     <img
                         src=""
                         alt="person"
@@ -124,7 +124,7 @@ function LeftNav(props) {
                         <h2>Profile</h2>
                     </Link>
                 </div>
-                <div>
+                <div className="left-nav-div">
                     <img
                         src=""
                         alt="people"
@@ -133,7 +133,7 @@ function LeftNav(props) {
                         <h2>Friends</h2>
                     </Link>
                 </div>
-                <div>
+                <div className="left-nav-div">
                     <img
                         src=""
                         alt="bone"
