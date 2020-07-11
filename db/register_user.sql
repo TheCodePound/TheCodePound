34a166e3054ed29cef03ea80376506a0a0cc5c93
jtown970 <jasonrtowner@gmail.com>
2020-07-10 22:10:42 -0700

    set up sql for updating the users info and started on update in auth but need to come back think out how to check to user password before able to change it

--- db/register_user.sql
+++ db/register_user.sql
@@ -1,2 +1,2 @@
 insert into users (full_name, email, password, profile_pic)
-values ( $1, $2, $3, $4 ) returning full_name, email, password, profile_pic;
+values ( $1, $2, $3, $4 ) returning full_name, email, profile_pic;
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -46,6 +46,30 @@ module.exports = {
     res.sendStatus(200)
   },
 
+  updateUserInfo: async (req, res) => {
+    const db = req.app.get('db')
+    const {id} = req.session.user
+    const {full_name, email, password, profile_pic} = req.body
+
+    // const checkPassword = 
+
+    // note start here when you get back 
+    // note might need to get users password off of session somehow and need to check if it matches the 
+    // pass word that the use input before they can change the password
+    // need to do a check if old password matches new password 
+    //   return new password set as the password then hash and salt it
+    // else return err 'wrong password'
+
+
+    const salt = bcrypt.genSaltSync(10)
+    const hash = bcrypt.hashSync(password, salt)
+
+    const newInfo = await db.update_user_info([[full_name, email, hash, profile_pic ]])
+
+
+
+  },
+
   getUser: async (req, res) => {
     if(req.session.user){
       res.status(200).send(req.session.user)
