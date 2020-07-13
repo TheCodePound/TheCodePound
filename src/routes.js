da85f65f8892dd34e3cb39a9768e18a51b10fdf5
Erickcamp <thecampkid@gmail.com>
2020-07-13 12:24:43 -0600

    added in protected routes

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,35 @@
-import React from 'react'
-import {Switch, Route} from 'react-router-dom'
-import Home from './components/Home'
-import Newpost from './components/Newpost.js'
-import Popup from './components/Popup.js'
-import Profile from './components/Profile'
-import Signin from './components/Signin.js'
-import Register from './components/Register.js'
+import React from "react"
+import { Switch, Route, Redirect } from "react-router-dom"
+import Home from "./components/Home"
+import Newpost from "./components/Newpost.js"
+import Popup from "./components/Popup.js"
+import Profile from "./components/Profile"
+import Signin from "./components/Signin.js"
+import Register from "./components/Register.js"
+import { useStore } from "react-redux"
 
-export default (
+const Routes = () => {
+  return (
     <Switch>
-        <Route exact path="/" component={Signin}/>
-        <Route path="/Home" component={Home}/>
-        <Route path="/Newpost" component={Newpost}/>
-        <Route path="/Popup/:post_id" component={Popup}/>
-        <Route path="/Profile" component={Profile}/>
-        <Route path="/Register" component={Register}/>
+      <Route exact path='/' component={Signin} />
+      <ProtectedRoute path='/Home' component={Home} />
+      <ProtectedRoute path='/Popup/:post_id' component={Popup} />
+      <ProtectedRoute path='/Profile' component={Profile} />
+      <ProtectedRoute path='/Register' component={Register} />
     </Switch>
-)
+  )
+
+  const ProtectedRoute = (props) => {
+    const {
+      userReducer: { user },
+    } = useStore().getState()
+
+    if (user.email === "") {
+      return <Redirect to='/' />
+    } else {
+      return <Route {...props} />
+    }
+  }
+}
+
+export default Routes
