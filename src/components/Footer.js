ad05e04b84fb76959de08a121507aed09c3e99ef
bbdoggett3 <bbdoggett3@gmail.com>
2020-07-13 14:18:13 -0600

    create style for homepage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,23 +4,26 @@ export default function Footer() {
 
     return (
         <div className="footer">
-            <h2>Follow us on</h2>
+            <h2 className="footer-text">Follow us on</h2>
             <div>
-                <a href="">
+                <a href="http://facebook.com">
                     <img 
-                        src=""
+                        className="social-icon"
+                        src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Ffacebook.png?v=1548445188791"
                         alt="facebook"
                     />
                 </a>
-                <a href="">
+                <a href="http://twitter.com">
                     <img
-                        src=""
+                        className="social-icon"
+                        src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2FTwitter.png?v=1548445203937"
                         alt="twitter"
                     />
                 </a>
-                <a href="">
+                <a href="https://instagram.com">
                     <img
-                        src=""
+                        className="social-icon"
+                        src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Finstagram-png-instagram-png-logo-1455.png?v=1593050365479"
                         alt="instagram"
                     />
                 </a>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,92 +1,106 @@
-import React, {useState, useEffect} from 'react'
-import {Link, withRouter} from 'react-router-dom'
-import {connect} from 'react-redux'
-import {logoutUser, getUser} from '../ducks/userReducer'
-import axios from 'axios'
-import '../styles/App.scss'
+import React, { useState, useEffect } from "react";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { logoutUser, getUser } from "../ducks/userReducer";
+import axios from "axios";
+import "../styles/App.scss";
 
 function Nav(props) {
-    
-    useEffect(() => {
-        getUser()
-    }, [])
+  useEffect(() => {
+    getUser();
+  }, []);
 
+  function login() {
+    axios
+      .post("/auth/login", { email, password })
+      .then((res) => {
+        props.loginUser(res.data);
+        props.history.push("/Home");
+      })
+      .catch((err) => {
+        alert("Username or password incorrect");
+      });
+  }
 
-const logout = () => {
-    axios.delete('/auth/logout')
-    .then(() => {
-        props.logoutUser()
-        props.history.push('/')
-    })}
+  const logout = () => {
+    axios.delete("/auth/logout").then(() => {
+      props.logoutUser();
+      props.history.push("/");
+    });
+  };
 
-const [search, setSearch] = useState("")
+  const [search, setSearch] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-function siteSearch() {
-    console.log("search")
+  function siteSearch() {
+    console.log("search");
+  }
+
+  const pathname = props.location.pathname;
+
+  if (pathname === "/") {
+    return (
+      <div className="signin-nav-container">
+        <div>
+          <img
+            className="nav-main-logo"
+            src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Ficons8-dog-64.png?v=1594436168204"
+            alt="Dog Logo"
+          />
+        </div>
+        <div className="signin-nav-inputs-container">
+          <input
+            className="signin-nav-inputs"
+            placeholder="Email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            className="signin-nav-inputs"
+            placeholder="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="signin-nav-btn" onClick={() => login()}>Sign In</button>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="nav-container">
+        <img
+          className="nav-main-logo"
+          src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Ficons8-dog-64.png?v=1594436168204"
+          alt="Dog Logo"
+        />
+        <div className="nav-search">
+          <img
+            className="nav-search-logo"
+            src=""
+            alt="Magnifying Glass"
+            onClick={() => siteSearch()}
+          />
+          <input
+            className="nav-search-text"
+            placeholder="Search Codepound"
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
+        <button className="nav-logout-btn" onClick={() => logout()}>
+          Logout
+        </button>
+      </div>
+    );
+  }
 }
 
-const pathname = props.location.pathname
+const mapStateToProps = (reduxState) => reduxState;
 
-if (pathname === '/') {
-    return (
-        <div className="signin-nav-container">
-            <div>
-                <img className="nav-main-logo" src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Ficons8-dog-64.png?v=1594436168204" alt="Dog Logo"/>
-            </div>
-            <div className="signin-nav-right">
-                <a href="#about">
-                    <h2>About</h2>
-                </a>
-                <Link to="/Register">
-                    <h2 className="signin-nav-link">Sign In</h2>
-                </Link>
-            </div>
-        </div>
-    )
-}
-
-else if (pathname === '/Register') {
-    return (
-        <div className="signin-nav-container">
-            <div>
-                <img className="nav-main-logo" src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Ficons8-dog-64.png?v=1594436168204" alt="Dog Logo"/>
-            </div>
-            <div className="signin-nav-right">
-                <a href="#about">
-                    <h2>About</h2>
-                </a>
-                <Link to="/">
-                    <h2 className="signin-nav-link">Sign In</h2>
-                </Link>
-            </div>
-        </div>
-    )
-}
-
-else {
-    return (
-        <div className="nav-container">
-            <img className="nav-main-logo" src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Ficons8-dog-64.png?v=1594436168204" alt="Dog Logo"/>
-            <div className="nav-search">
-                <img
-                    className="nav-search-logo" 
-                    src="" 
-                    alt="Magnifying Glass"
-                    onClick={() => siteSearch()}
-                />    
-                <input
-                    className="nav-search-text" 
-                    placeholder="Search Codepound"
-                    type="text"
-                    value={search}
-                    onChange={e => setSearch(e.target.value)}
-                />
-            </div>
-            <button className="nav-logout-btn" onClick={() => logout()}>Logout</button>
-        </div>
-    )
-}}
-
-const mapStateToProps = reduxState => reduxState
-
-export default withRouter(connect(mapStateToProps, {logoutUser, getUser})(Nav))
+export default withRouter(
+  connect(mapStateToProps, { logoutUser, getUser })(Nav)
+);
