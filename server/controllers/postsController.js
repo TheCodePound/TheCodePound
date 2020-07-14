ee4ab2a913f6a9341609bc296574eb373c5b9922
jtown970 <jasonrtowner@gmail.com>
2020-07-14 10:54:01 -0700

    took out console.log

--- server/controllers/postsController.js
+++ server/controllers/postsController.js
@@ -4,8 +4,10 @@ module.exports = {
   createPost: async (req, res) => {
     const db = req.app.get('db')
     const {user_id} = req.session.user
+    // const {user_post_id} = req.params
     const { user_post_id, title, content, post_img, languages} = req.body
-
+    // console.log(id)
+    // console.log(req.session.user)
     const newPost = await db.add_post([user_id, user_post_id, title, content, post_img, languages]);
       return res.status(200).send(newPost)
 
