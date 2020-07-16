f2997e07bb09bd41dfa5199865d762fe0e43c5c5
paulbrooks646 <paulbrooks646@gmail.com>
2020-07-16 16:09:15 -0600

    bugs

--- db/update_user_info.sql
+++ db/update_user_info.sql
@@ -1,9 +1,9 @@
 update users
 set full_name = $2, email = $3, new_email = $4, password = $5, new_password = $6, profile_pic = $7
-where id = $1;
+where user_id = $1;
 
 update users
 set email = $4, password = $6
-where id = $1
+where user_id = $1
 returning full_name, email, password, profile_pic;
 
