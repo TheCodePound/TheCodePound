b163c8345b86fe404615cf65ce86dafd450d0786
Erickcamp <thecampkid@gmail.com>
2020-07-16 13:25:44 -0600

    testing with paul on getting our information to be pulled from the db. some parts are working. want to send up so we can both work seperately on it to figure it out more.

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,79 +5,103 @@ import { getPosts } from "../ducks/postReducer"
 
 const Home = ({ postReducer, posts, getPosts, ...props }) => {
   const [loading, setLoading] = useState(true)
-  // const [title, setTitle] = useState("")
-  // const [img, setImg] = useState('')
-  // const [content, setContrent] = useState('')
+  const [title, setTitle] = useState("")
+  const [img, setImg] = useState("")
+  const [content, setContent] = useState("")
 
-  // commenting out until post controller is built out so it dosent break anything.
-  // useEffect(() => {
-  //     axios
-  //     .get('/api/posts')
-  //     .then((res) => {
-  //         getPosts(res.data)
-  //     })
-  //     .catch((err) => {
-  //         console.log(err)
-  //     })
-  //     .finally(() => setLoading(false))
-  // }, [])
+  useEffect(() => {
+    axios
+      .get("/api/all/posts")
+      .then((res) => {
+        getPosts(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+      .finally(() => setLoading(false))
+  }, [])
 
-  // function handleTitle(e) {
-  //   setTitle(e.target.value)
-  // }
+  function handleTitle(e) {
+    setTitle(e.target.value)
+  }
 
-  // function handleImg(e) {
-  //   setImg(e.target.value)
-  // }
+  function handleImg(e) {
+    setImg(e.target.value)
+  }
 
-  // function handleContent(e) {
-  //   setContent(e.target.value)
-  // }
+  function handleContent(e) {
+    setContent(e.target.value)
+  }
 
-  // function handleSubmit(e) {
-  //   e.preventDefault()
-  //   axios
-  //     .post("/api/posts", { title, img, content, fullname: props.userReducer.user.fullname })
-  //     .then((res) => {
-  //       props.history.push("/Home")
-  //     })
-  //     .catch((err) => {
-  //       console.log(err)
-  //     })
-  // }
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log("hit")
+    axios
+      .post("/api/post", { title, content })
+      .then((res) => {
+        props.history.push("/Home")
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
 
-  // dont know if we will want this button or not. Might add some good user experience.
-  // function cancel() {
-  //   props.history.push("/Home")
-  // }
+  function cancel() {
+    props.history.push("/Home")
+  }
 
   return (
-    <div className="home-container">
+    <div className='home-container'>
       <div className='pound-container'>
         {/* {props.user.languages} */}
-        <img className="language-image" src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Fjavascript-icon.png?v=1594835510447" alt="programming language icon" />
+        <img
+          className='language-image'
+          src='https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Fjavascript-icon.png?v=1594835510447'
+          alt='programming language icon'
+        />
         <div className='pound-text-details'>
           {/* {props.user.profile_pic} */}
-          <img className="pound-profile-pic" src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2FFamily%20Pic%202018%202.jpg?v=1546837304267" alt="profile"/>
-          <textarea className="pound-text-input" 
-                    placeholder='Project details here...' 
+          <img
+            className='pound-profile-pic'
+            src='https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2FFamily%20Pic%202018%202.jpg?v=1546837304267'
+            alt='profile'
+          />
+          <input
+            className='pound-text-input'
+            placeholder='Project details here...'
+            value={content}
+            onChange={handleContent}
           />
         </div>
         <div className='pound-icons-btns'>
           <div className='pound-icons'>
-            <img className="icon-image" src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Ficon%20image%20BIG.png?v=1594831414804" alt="image icon" />
-            <img className="icon-image" src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Fgif%20icon%20big.png?v=1594831425259" alt="gif icon"/>
-            <img className="icon-image" src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Fvideo%20icon%20big.png?v=1594831421374" alt="video icon"/>
+            <img
+              className='icon-image'
+              src='https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Ficon%20image%20BIG.png?v=1594831414804'
+              alt='image icon'
+            />
+            <img
+              className='icon-image'
+              src='https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Fgif%20icon%20big.png?v=1594831425259'
+              alt='gif icon'
+            />
+            <img
+              className='icon-image'
+              src='https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Fvideo%20icon%20big.png?v=1594831421374'
+              alt='video icon'
+            />
           </div>
           <div className='pound-btns'>
-            <button className="pound-individual-btn">Add Language +</button>
-            <button className="pound-individual-btn">Post Pound</button>
+            <button className='pound-individual-btn'>Add Language +</button>
+            <button className='pound-individual-btn' onClick={handleSubmit}>
+              Post Pound
+            </button>
           </div>
         </div>
       </div>
       <div>
         {!loading ? (
-          postReducer.posts.map((el, index) => {
+          posts.posts.map((el, index) => {
             return (
               <div key={el?.id ?? index} onClick={() => props.history.push(`/Popup/${el.post_id}`)}>
                 <div>
