87400c923fcedca7a26249ad8a24be39ae099dde
jtown970 <jasonrtowner@gmail.com>
2020-07-17 12:24:35 -0700

    able to add title content img and languages all in one post

--- db/get_all_post.sql
+++ db/get_all_post.sql
@@ -1,7 +1,12 @@
 
-select users.full_name, users.profile_pic, posts.title, posts.content, posts.post_id, img.img, languages.languages, languages.languages_img, comments.comments, bones.bones from posts
+-- select users.full_name, users.profile_pic, posts.title, posts.content, posts.post_id, img.img, languages.languages, languages.languages_img, comments.comments, bones.bones from posts
+-- join users on posts.user_id = users.user_id
+-- join img on posts.post_id = img.post_img_id
+-- join languages on posts.post_id = post_languages_id
+-- join comments on posts.post_id = post_comments_id
+-- join bones on posts.post_id = post_bones_id
+
+select users.full_name, users.profile_pic, posts.title, posts.content, posts.post_id, img.img, languages.languages, languages.languages_img from posts
 join users on posts.user_id = users.user_id
 join img on posts.post_id = img.post_img_id
 join languages on posts.post_id = post_languages_id
-join comments on posts.post_id = post_comments_id
-join bones on posts.post_id = post_bones_id
