fd68918ef72a6dc55d9e8c107d9215f7d7b81419
jtown970 <jasonrtowner@gmail.com>
2020-07-20 13:04:54 -0700

    able to get comments and bones for post by id note this has been tested and works

--- db/get_post_by_id.sql
+++ db/get_post_by_id.sql
@@ -1,10 +1,16 @@
-select * from junction j
-join users u
-on u.user_id = j.user_junction_id 
-join posts p
-on p.post_id = j.post_junction_id
-join img i
-on i.post_img_id = j.post_junction_id  
-join languages l
-on l.post_languages_id = j.post_junction_id
-where p.post_id = $1;
+-- select * from junction j
+-- join users u
+-- on u.user_id = j.user_junction_id 
+-- join posts p
+-- on p.post_id = j.post_junction_id
+-- join img i
+-- on i.post_img_id = j.post_junction_id  
+-- join languages l
+-- on l.post_languages_id = j.post_junction_id
+-- where p.post_id = $1;
+
+select users.full_name, users.profile_pic, posts.title, posts.content, posts.post_id, img.img, languages.languages, languages.languages_img from posts
+join users on posts.user_id = users.user_id
+join img on posts.post_id = img.post_img_id
+join languages on posts.post_id = post_languages_id
+where posts.post_id = $1;
