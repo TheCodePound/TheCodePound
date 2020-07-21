e8c315c4a558566db5330f7fa86ce50be861bb92
jtown970 <jasonrtowner@gmail.com>
2020-07-21 09:37:52 -0700

    have a time stamp for posts, changed comments to title on popup and altered db files for add_post to have date and to have a profile pic think the pic should already be up tho

--- server/controllers/postsController.js
+++ server/controllers/postsController.js
@@ -17,9 +17,11 @@ module.exports = {
   allInOnePost: async (req, res) => {
     const db = req.app.get("db")
     const { user_id } = req.session.user
+    const d = Date()
+    const post_date = d.slice(0,15)
     const { title, content, img, languages, languages_img } = req.body
 
-    const newPost = await db.add_post({ user_id, title, content })
+    const newPost = await db.add_post({ user_id, title, content, post_date })
     const user_junction_id = req.session.user.user_id
     const post_junction_id = newPost[0].post_id
     await db.get_post_id_in_junction([user_junction_id, post_junction_id])
