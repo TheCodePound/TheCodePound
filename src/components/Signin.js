88792f54c3d55cecf840541b3018953bafc3c9ed
paulbrooks646 <paulbrooks646@gmail.com>
2020-07-23 16:51:35 -0600

    full_name

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -8,7 +8,7 @@ import { connect } from "react-redux"
 function Register(props) {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
-  const [fullname, setFullname] = useState("")
+  const [full_name, setFullname] = useState("")
   const [confirmPassword, setConfirmPassword] = useState("")
 
   function register() {
@@ -54,7 +54,7 @@ function Register(props) {
                 className='signin-bone-fields'
                 placeholder='Full Name'
                 type='text'
-                value={fullname}
+                value={full_name}
                 onChange={(e) => setFullname(e.target.value)}
               />
               <input
